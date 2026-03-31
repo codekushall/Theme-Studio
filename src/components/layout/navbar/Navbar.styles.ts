f0f057553f439053ecd1355ec77e8e9efe4b6898
codekushall <shresthakushal913@gmail.com>
2026-03-31 12:01:14 +0545

    button added

--- src/components/layout/navbar/Navbar.styles.ts
+++ src/components/layout/navbar/Navbar.styles.ts
@@ -1,25 +1,32 @@
 import styled from "styled-components";
 
 export const NavbarWrapper = styled.nav`
-   height: 64px;
-   width: 100%;
-   padding:0px 40px;
-   display: flex;
-   justify-content: space-between;
-   border: 1px solid;
-   align-items:center;
+  height: 64px;
+  width: 100%;
+  padding: 0px 40px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid;
+  align-items: center;
 `;
 export const HeadingOne = styled.h1`
-  color:blue;
-  font-weight:800;
+  color: blue;
+  font-weight: 800;
+`;
+
+export const ButtonWrapper = styled.button`
+  align-items: center;
+  justify-content: center;
+  display: flex;
 `;
 export const PreviewButton = styled.button`
   padding: 8px 12px;
-  font-size:16px;
-  font-weight:800;
+  font-size: 16px;
+  font-weight: 800;
   color: white;
-   background-color:red;
-   border-radius: 6px;
+  background-color: black;
+  border-radius: 6px;
+  display: flex;
 `;
 export const ExportButton = styled.button`
   padding: 8px 12px;
@@ -28,4 +35,5 @@ export const ExportButton = styled.button`
   border: 1px solid;
   margin: 0px 0px 0px 10px;
   border-radius: 6px;
+  display: flex;
 `;
--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 import { DropDown, EyeIcon } from "../../icon/Icon";
-import { ExportButton, HeadingOne, NavbarWrapper, PreviewButton } from "./Navbar.styles";
+import { ButtonWrapper, ExportButton, HeadingOne, NavbarWrapper, PreviewButton } from "./Navbar.styles";
 
 const Navbar: React.FC = () => {
   return (
     <NavbarWrapper>
       <HeadingOne>Theme-Studio</HeadingOne>
-      <div>
-        <PreviewButton> <EyeIcon color="none"/> Preview</PreviewButton>
+      <ButtonWrapper>
+        <PreviewButton> <EyeIcon color="white" /> Preview</PreviewButton>
         <ExportButton>Export<DropDown /></ExportButton>
-      </div>
+      </ButtonWrapper>
     </NavbarWrapper>
   );
 };
