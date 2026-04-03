05c4fbff9a21d9ec03548a9c4408b73e3d6284f3
codekushall <shresthakushal913@gmail.com>
2026-04-03 17:21:06 +0545

    icons added

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
+import { SearchIcon } from "../icon/Icon";
+
 const SideBar: React.FC = () => {
     return ( 
         <div>
-        
+        <SearchIcon /> <input  type="fill" placeholder="Seacrh Components..."/>
+        <h4>COMPONENTS</h4>
         </div>
     );
 };
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/layout/navbar/Navbar";
+import SideBar from "../components/sidebar/Sidebar";
 
 const RootLayout = () => (
   <>
     <Navbar />
     <Outlet />
+    <SideBar />
     <TanStackRouterDevtools />
   </>
 );
